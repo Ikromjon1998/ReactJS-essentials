0faec065ecb5b9dfb790f36f68e3ea63085f0847
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-10-25 22:05:23 +0500

    Section 5 is completed

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -8,18 +8,30 @@ import React, { useEffect, useReducer, useState } from 'react';
 
 function App({login}) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://api.github.com/users/" + login).then(response => response.json()).then(setData);
-  }, []);
-  if(data) {
-    return (<div>
+    if(!login) return;
+    setLoading(true);
+    fetch("https://api.github.com/users/" + login)
+    .then(response => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if(!data) return null;
+  return (
+    <div>
       <h1>{data.name}</h1>
       <p>{data.location}</p>
       <img alt={data.login} src={data.avatar_url} />
-    </div>);
-  }
-  return <div>No User Available</div>
+    </div>
+  );
+  
 }
 
 export default App;
