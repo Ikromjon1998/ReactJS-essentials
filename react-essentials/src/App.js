cc00495fe68aaedfb638d1eb9816668027f185f0
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-10-25 21:48:53 +0500

    Section 5.2 is "Displaying data from an API"

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -13,7 +13,11 @@ function App({login}) {
     fetch("https://api.github.com/users/" + login).then(response => response.json()).then(setData);
   }, []);
   if(data) {
-    return <div>{JSON.stringify(data)}</div>
+    return (<div>
+      <h1>{data.name}</h1>
+      <p>{data.location}</p>
+      <img alt={data.login} src={data.avatar_url} />
+    </div>);
   }
   return <div>No User Available</div>
 }
