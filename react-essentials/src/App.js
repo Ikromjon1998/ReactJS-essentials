0b9dd473834c8744033e283a8f9f0a3fb1386ed7
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-10-25 21:03:44 +0500

    Section 4.5 is Working with useEffect

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,17 +1,26 @@
 //import logo from './logo.svg';
 import './App.css';
-import restaurant from './restaurant.jpeg'
-import React, { useState } from 'react';
+//import restaurant from './restaurant.jpeg'
+import React, { useEffect, useState } from 'react';
 
 
 
 
 function App() {
   const [emotion, setEmotion] = useState("Happy");
+  const [secondary, setSecondary] = useState("tired");
+  useEffect(() => {
+    console.log("It's " + emotion + " around here!");
+  }, [emotion]);
+  useEffect(() => {
+    console.log("It's " + secondary + " around here!");
+  }, [secondary])
+
   console.log(emotion)
   return <>
-    <h1>Current emotion: {emotion}.</h1>
-    <button onClick={() => setEmotion("happy")}>Happy</button>
+    <h1>Current emotion: {emotion} and {secondary}.</h1>
+    <button onClick={() => setEmotion("happy")}>Make Happy</button>
+    <button onClick={() => setSecondary("crabby")}>Make crabby</button>
     <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
     <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
   </>
