2f9513072be19ed963297624b9c8579a0ae6e0d3
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-10-25 18:16:14 +0500

    Section 3 is completed.

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { buildQueries } from '@testing-library/dom';
+// import { buildQueries } from '@testing-library/dom';
 // import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  React.createElement(
-    "ul", 
-    {style: {color: "blue"}}, 
-    React.createElement("li", null, "Monday"),
-    React.createElement("li", null, "Tuesdey"),
-    React.createElement("li", null, "Wednesdey"),
-    React.createElement("li", null, "Thursday")
-  ),
+  <App />,
   document.getElementById('root')
 );
 
@@ -21,4 +15,3 @@ ReactDOM.render(
 //   React.createElement("h1", {style: {color: "blue"}}, "Hello everyone!"),
 //   document.getElementById('root')
 // );
-
