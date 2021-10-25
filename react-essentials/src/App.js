38c97229ef510fab97566cc07db6044b827081d9
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-10-25 18:27:17 +0500

    Section 4.1 is conditional rendering

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -2,65 +2,19 @@
 import './App.css';
 import restaurant from './restaurant.jpeg'
 
-function Header(props) {
-  return(
-    <header>
-      <h1>{props.name}'s kitchen</h1>
-    </header>
-  );
-}
-function Main (props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjective} food around the world!</p>
-      <img 
-        src={restaurant} 
-        height={300}
-        alt="Interier in a restaurant"  
-      />
 
-      <ul style={{textAlign: 'left'}}>
-        {
-          props.dishes.map(
-            (dish) => <li key={dish.id}>{dish.title}</li>
-          )
-        }
-      </ul>
-    </section>
-  )
-}
-function Footer (props){
-  return (
-    <footer>
-      <p>Copyright {props.year}.</p>
-    </footer>
-  );
+function SecretComponent (){
+  return <h1>Secret information for authorized users only</h1>
 }
 
-const dishes = [
-  "Osh", 
-  "Lagman",
-  "Manpar", 
-  "Chuchvara"
-];
+function RegularComponent(){
+  return <h1>Everyone can see this component.</h1>
+}
 
-const dishObjects = dishes.map((dish, i) => ({
-  id: i, 
-  title: dish
-}));
-// console.log(dishObjects);
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Ikromjon" />
-      <Main adjective="amazing" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+function App(props) {
+  <>
+    {props.authorized ? <SecretComponent /> : <RegularComponent />}
+  </>
 }
 
 export default App;
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 
 ReactDOM.render(
-  <App />,
+  <App authorized={true} />,
   document.getElementById('root')
 );
 
