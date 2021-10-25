9efd4b3f7aa71cf27eadb2d68d665c96beb92f9b
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-10-25 21:17:38 +0500

    Section 4.6 is "Importing useReducer"

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,29 +1,40 @@
 //import logo from './logo.svg';
 import './App.css';
 //import restaurant from './restaurant.jpeg'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 
 
 
 
 function App() {
-  const [emotion, setEmotion] = useState("Happy");
-  const [secondary, setSecondary] = useState("tired");
-  useEffect(() => {
-    console.log("It's " + emotion + " around here!");
-  }, [emotion]);
-  useEffect(() => {
-    console.log("It's " + secondary + " around here!");
-  }, [secondary])
+  const [checked, toggle] = useReducer(
+    (checked) =>  !checked,false
+  );
 
-  console.log(emotion)
   return <>
-    <h1>Current emotion: {emotion} and {secondary}.</h1>
-    <button onClick={() => setEmotion("happy")}>Make Happy</button>
-    <button onClick={() => setSecondary("crabby")}>Make crabby</button>
-    <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-    <button onClick={() => setEmotion("enthusiastic")}>Enthuse</button>
+    <input 
+      type="checkbox" 
+      value={checked} 
+      onChange={toggle} />
+    <h1>This is {checked ? "checked" : "not checked"}.</h1>
   </>
 }
 
 export default App;
+
+
+// function App() {
+//   const [checked, setChecked] = useState(false);
+
+//   function toggle(){
+//     setChecked((checked) =>  !checked);
+//   }
+
+//   return <>
+//     <input 
+//       type="checkbox" 
+//       value={checked} 
+//       onChange={toggle} />
+//     <h1>This is {checked ? "checked" : "not checked"}.</h1>
+//   </>
+// }
